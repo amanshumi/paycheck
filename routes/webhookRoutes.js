58a0c80535dc49d09b9eadc2dcdff8b9adb81682
Octavian47 <109802262+Octavian47@users.noreply.github.com>
2024-02-18 20:00:53 +0200

    Update webhookRoutes.js

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { handleWebhook } = require('./webhookController');
+const { handleWebhook } = require('../controllers/webhookController');
 
 router.post('/webhook', handleWebhook);
 
