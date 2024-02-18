3d66e3bbcd8f26a968cc75b084ce7d0b40135315
Octavian47 <109802262+Octavian47@users.noreply.github.com>
2024-02-18 20:05:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT
 
-//TEST AUTO DEPLOY
+//TEST AUTO DEPLOYy
 
 // Middleware to parse JSON bodies
 app.use(express.json());
