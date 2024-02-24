67ae9ade041f244ddbb6e4533c69b6ed8408982a
Tavi <octavian.dragu47@gmail.com>
2024-02-24 17:36:08 +0200

    Create Group Logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,28 @@
-// controllers/userController.js
-const { User } = require('../models');
+const { validationResult } = require('express-validator');
 
+//Santier and Debug
+const fs = require('fs');
+const path = require('path');
+
+// Function to append errors to log.txt
+const logErrorToFile = (error) => {
+  const logMessage = `${new Date().toISOString()} - Error: ${error.message}\n`;
+  const logFilePath = path.join(__dirname, '..', 'logs', 'log.txt'); // Adjust path as needed
+
+  // Append the error message to the log file, creating the file if it doesn't exist
+  fs.appendFile(logFilePath, logMessage, (err) => {
+    if (err) console.error('Logging Error:', err);
+  });
+};
+
+
+
+// Requrie Models
+const { User, Group, GroupMember } = require('../models');
+const { sequelize } = require('../models');
+
+
+//Get user info
 exports.getUserInfo = async (req, res) => {
   try {
     //get user id from token
@@ -21,3 +43,52 @@ exports.getUserInfo = async (req, res) => {
     res.status(500).json({ message: 'An error occurred while fetching user information' });
   }
 };
+
+//Create group
+exports.createGroup = async (req, res) => {
+
+  // Check for validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+ 
+  // Start a new transaction
+  const transaction = await sequelize.transaction(); 
+  try {
+    const { name, description, currency } = req.body;
+
+    // User ID from authentication middleware
+    const createdByUserId = req.user.id; 
+
+    // Create new group with transaction
+    const group = await Group.create({
+      name,
+      description,
+      currency,
+      createdByUserId,
+    }, { transaction });
+
+    // Automatically add creator as a member of the group
+    await GroupMember.create({
+      userId: createdByUserId,
+      groupId: group.id,
+      joined_at: new Date() 
+    }, { transaction });
+
+    // Commit the transaction
+    await transaction.commit(); 
+
+    res.status(201).json({ name: group.name });
+
+  } catch (error) {
+    // Roll back the transaction in case of error
+    await transaction.rollback(); 
+    console.error('Create Group Error:', error);
+    //Log the errors
+    logErrorToFile(error);
+    res.status(500).json({ message: 'An error occurred while creating the group' });
+  }
+};
+
+
