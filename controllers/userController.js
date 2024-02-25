36867a5d64e3e2e7ff813caed0d4052e9fcf54cc
Tavi <octavian.dragu47@gmail.com>
2024-02-25 14:02:27 +0200

    Update the create group return info

--- controllers/userController.js
+++ controllers/userController.js
@@ -79,7 +79,12 @@ exports.createGroup = async (req, res) => {
     // Commit the transaction
     await transaction.commit(); 
 
-    res.status(201).json({ name: group.name });
+    res.status(201).json({ 
+      name: group.name,
+      currency : group.currency,
+      //Doar unu, ca doar ce facu grupul
+      membersCount: 1, 
+    });
 
   } catch (error) {
     // Roll back the transaction in case of error
